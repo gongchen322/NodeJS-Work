ac45fc8ee0fcd6e61f8045610f9f93f4b1bdc479
gongchen322 <gong27@purdue.edu>
2016-01-27 21:59:35 -0800

    todo

---
+++ todo/server.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+	res.send('Todo API Root');
+});
+
+//Get /todos
+app.get('/todos', function (req,res) {
+	res.json(todos);
+})
+
+
+//Get /todos/:id
+app.get('/todos/:id', function (req,res) {
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo;
+	//Iterate of todos array
+
+	todos.forEach(function (todo) {
+		if(todoId === todo.id) {
+			matchedTodo = todo;
+		}
+	})
+
+	if(matchedTodo) {
+		res.json(matchedTodo);
+	} else {
+		res.status(404).send();
+	}
+
+	res.send('Asking for todo with id of '+ req.params.id)
+});
+
+//POST /todos
+
+app.post('/todos', function (req,res) {
+	var body = req.body;
+	body.id = todoNextId++;
+	todos.push(body);
+	res.json(body);
+});
+
+
+
+app.listen(PORT, function () {
+	console.log('Express listening on port '+ PORT + '!');
+});
